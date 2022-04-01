bba5b8a380179f72ed9f515c93c376d986711a5e
svfmb <hanokendigeri@gmail.com>
2022-04-01 15:59:09 +0530

    updated students' information

--- images/students/resize.py
+++ images/students/resize.py
@@ -6,9 +6,11 @@ for f in os.listdir('.'):
     if f.endswith('.png') or f.endswith('.jpeg') or f.endswith('.jpg'):
         image = Image.open(f)
         width, height = image.size
-        new_width = 100
+        new_width = 120
         new_height = int((height*new_width) / width)
-        image = image.resize((new_width, new_height))
+        image = image.resize((new_width, new_height), Image.ANTIALIAS)
+        if new_height > 120:
+            image = image.crop((0, 0, new_width, 120))
         fn, fext = os.path.splitext(f)
-        image.save('resized/' + fn + '_resized' + fext, quality=90)
+        image.save('resized/' + fn + '_resized' + fext, quality=95)
 
